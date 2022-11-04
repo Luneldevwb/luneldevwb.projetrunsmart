a1c9ae04c10e9e7cf132d2834aed20d5164fbee6
Lunel <nematt007@gmail.com>
2022-11-04 09:49:53 +0300

    added slick for slider

---
+++ src/js/script.js
@@ -0,0 +1,15 @@
+var name = 'Lunel';
+let number = 7;
+const pi = 3.14;
+
+number = 4;
+
+// alert(11);
+
+// console.log(number);
+// let answer = confirm('You?');
+// console.log(answer);
+
+let answer = prompt('You?', '');
+console.log(answer);
+
