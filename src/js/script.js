ad501fab6de295ae631edb5234e51c68e2e90cd3
Lunel <nematt007@gmail.com>
2022-11-04 09:51:32 +0300

    new mod

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,45 @@ number = 4;
 // let answer = confirm('You?');
 // console.log(answer);
 
-let answer = prompt('You?', '');
-console.log(answer);
+// let answer = prompt('You?', '');
+// console.log(answer);
+// let isChecked = true,
+//     isClose  = false;
+// // console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+// if(2*4 == 8*3) {
+//     console.log('Is true');
+
+// } else{
+//     console.log('Error');
+// }
+
+// let answer = confirm('Are you 18?');
+// if(answer){
+// console.log('Get in!');
+// } else{
+//     console.log('Go away!')
+// }
+
+// const num = 60;
+// if(num < 49) {
+//     console.log('Not true');
+// } 
+// else if(num > 100) {
+//     console.log('A lot');
+// }
+// else{
+//     console.log('Right');
+// }
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(i);
+// }
+function logging(a, b) {
+    console.log(a + b);
+
+}
+
+logging(3, 4);
+logging(7, 3);
 
