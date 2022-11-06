2a11280779ad2c217fad01ffa7c5ecd6b293ba2d
Lunel <nematt007@gmail.com>
2022-11-06 07:52:48 +0300

    added new settings for carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
     $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: false,
+        prevArrow:'<button type="button" class="slick-prev"><img src="../icons/previous_icon.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next_icon.png"></button>',
+        responsive:[
+            {
+                breakpoint: 992,
+                settings: {
+                  dots: true,
+                  arrows: false
+                }
+            }
+        ]
+        
     });
   });
