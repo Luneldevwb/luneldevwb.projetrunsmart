b735d6ddcd09c3f346276c32295a07b0df5e7482
Lunel <nematt007@gmail.com>
2022-11-07 09:15:41 +0300

    added scripts for Tiny slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,38 @@
-$(document).ready(function(){
-    $('.carousel__inner').slick({
-        speed: 1200,
-        adaptiveHeight: true,
-        autoplay: false,
-        prevArrow:'<button type="button" class="slick-prev"><img src="../icons/previous_icon.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/next_icon.png"></button>',
-        responsive:[
-            {
-                breakpoint: 992,
-                settings: {
-                  dots: true,
-                  arrows: false
-                }
-            }
-        ]
+// $(document).ready(function(){
+//     $('.carousel__inner').slick({
+//         speed: 1200,
+//         adaptiveHeight: true,
+//         autoplay: false,
+//         prevArrow:'<button type="button" class="slick-prev"><img src="icons/previous_icon.png"></button>',
+//         nextArrow: '<button type="button" class="slick-next"><img src="icons/next_icon.png"></button>',
+//         responsive:[
+//             {
+//                 breakpoint: 992,
+//                 settings: {
+//                   dots: true,
+//                   arrows: false
+//                 }
+//             }
+//         ]
         
-    });
-  });
+//     });
+//   });
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false
+    
+});
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+  }); 
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+  }); 
+  
+
