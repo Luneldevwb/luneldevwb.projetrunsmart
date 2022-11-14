d1ea3b3fce04e78253563b385a51fd195f2fb43a
Lunel <nematt007@gmail.com>
2022-11-14 10:08:34 +0300

    added commentary

--- src/js/script.js
+++ src/js/script.js
@@ -40,16 +40,17 @@ $(document).ready(function () {
     //     });
     // });
 
+    //Pour l'optimisation du code
     function toggleSlide(item) {
         $(item).each(function (i) {
             $(this).on('click', function (e) {
                 e.preventDefault();
                 $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
                 $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
-            })
+            });
 
         });
-    };
+    }
 
     toggleSlide('.catalog-item__link');
     toggleSlide('.catalog-item__back');
