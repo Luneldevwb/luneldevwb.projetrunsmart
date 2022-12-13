c9c16ba9ce4a5b5094b19f013e0ab160c63d83fa
Lunel <nematt007@gmail.com>
2022-12-13 10:20:31 +0300

    Added new script for modal.

--- src/js/script.js
+++ src/js/script.js
@@ -63,6 +63,24 @@ $(document).ready(function () {
     toggleSlide('.catalog-item__back');
 
     //Modal
-    
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
+
+    // $('.button_mini').on('click', function() {
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+            
+        });
+    });
+
 
 });
