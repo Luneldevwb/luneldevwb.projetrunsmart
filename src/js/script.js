3ebfe44b059bcf0b9ff2dd817d3c82fb67f0e4c6
Lunel <nematt007@gmail.com>
2022-12-16 10:15:39 +0300

    new mod form validation and inputmask

--- src/js/script.js
+++ src/js/script.js
@@ -81,6 +81,67 @@ $(document).ready(function () {
             
         });
     });
+    // $('#consultation-form').validate();
+    // $('#consultation form').validate({
+    //     rules: {
+    //         name: {
+    //             required: true,
+    //             minlength: 2
+    //         },
+    //         phone: 'required',
+    //         email: {
+    //             required: true,
+    //             email: true
+    //         }
+    //     },
+    //     messages: {
+    //         name: {
+    //             require: "Please specify your name",
+    //             minlength: jQuery.validator.format("At least {0} characters required!")
+    //         }, 
+    //         phone: "Please specify your phone",
+    //         email: {
+    //           required: "We need your email address to contact you",
+    //           email: "Your email address must be in the format of name@domain.com"
+    //         }
+    //     }
+    // });
+    // $('#order form').validate();
 
+
+    //Script universel pour toutes mes formes
+    function valideForms(form) {
+        $(form).validate({
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                phone: 'required',
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    require: "Please specify your name",
+                    minlength: jQuery.validator.format("At least {0} characters required!")
+                }, 
+                phone: "Please specify your phone",
+                email: {
+                  required: "We need your email address to contact you",
+                  email: "Your email address must be in the format of name@domain.com"
+                }
+            }
+        });
+    }
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form');
+
+    // $('#phone').mask("+7 (999) 999-99-99");
+    $("input[name=phone]").inputmask("+7 (999) 999-99-99");
+    $("input[name=email]").inputmask("email");
 
 });
