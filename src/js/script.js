dc0c3b053d11b115654e555de7895d28ae9e0ef2
Lunel <nematt007@gmail.com>
2022-12-21 10:55:32 +0300

    new scripts added

--- src/js/script.js
+++ src/js/script.js
@@ -151,12 +151,12 @@ $(document).ready(function () {
             return;
         }
         $.ajax({
-            type: 'POST',
-            url: 'mailer/smart.php',
+            type: "POST",
+            url: "mailer/smart.php",
             data: $(this).serialize()
 
         }).done(function() {
-            $(this).find('input').val('');
+            $(this).find("input").val("");
             $('#consultation, #order').fadeOut();
             $('.overlay, #thanks').fadeIn('slow');
             $('form').trigger('reset');
@@ -164,5 +164,21 @@ $(document).ready(function () {
         return false;
 
     });
+
+    //Smooth scroll and page up
+    $(window).scroll(function() {
+        if($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+
 
 });
